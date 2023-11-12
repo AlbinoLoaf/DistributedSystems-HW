4e68dabe83b619eb1167250b57c4947dfd581ca9
Marius Thomsen <mariu@itu.dk>
2023-11-12 21:16:34 +0100

    Torn down

--- HW04/Node/node.go
+++ HW04/Node/node.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"net"
 	"strings"
 	"time"
@@ -25,6 +24,7 @@ var Address = flag.String("address", "localhost", "Tcp server address")
 type Server struct {
 	proto.UnimplementedNodeManagementServer // You need this line if you have a server
 	id                                      int64
+	Timestamp                               int64
 	usingCriticalSection                    bool
 }
 
@@ -32,7 +32,11 @@ func main() {
 	flag.Parse()
 	fmt.Println("Starting server...")
 	// log.SetOutput(os.Stdout)
-
+	server := &Server{
+		id:                   *idf,
+		Timestamp:            1,
+		usingCriticalSection: false,
+	}
 	// Create a listener on TCP port
 	lis, err := net.Listen("tcp", *Address+":"+*Port)
 	if err != nil {
@@ -43,14 +47,14 @@ func main() {
 	s := grpc.NewServer()
 	fmt.Printf("Server type: %T\n", s)
 	// Attach the Node service to the server
-	proto.RegisterNodeManagementServer(s, &Server{id: *idf, usingCriticalSection: false})
+	proto.RegisterNodeManagementServer(s, server)
 
 	// start sending requests to peer
 	list := GetPorts()
 	i := 0
 	for i < len(list) {
 		if !strings.Contains(list[i], *Port) {
-			go runClient(list[i], s)
+			go runClient(list[i])
 
 		}
 		i++
@@ -63,7 +67,32 @@ func main() {
 	// code here is unreachable because protoServer.Serve occupies the current thread.
 }
 
-func runClient(connection string, server *grpc.Server) {
+func (s *Server) RequestReply(ctx context.Context, in *proto.Request) (*proto.Reply, error) {
+	// If true this node accepts the requests replying go ahead
+	replyMSG := fmt.Sprintf("Saying hi from %d", s.id)
+	return &proto.Reply{Message: replyMSG, Id: s.id}, nil
+}
+func (s *Server) ResolveRequest(ForeignTimestamp int64, foreignID int64) (Accaptance bool) {
+	fmt.Printf("Server %d got contacted by server %d\n", s.id, foreignID)
+	for s.usingCriticalSection {
+		time.Sleep(time.Millisecond * 500)
+	}
+	return true
+
+}
+
+func GetPorts() []string {
+	content, err := ioutil.ReadFile("ports.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	portString := bytes.NewBuffer(content).String()
+	portArr := strings.Split(portString, ",")
+
+	return portArr
+}
+
+func runClient(connection string) {
 	fmt.Println("starting client...")
 	// Set up a connection to the server.
 	opts := []grpc.DialOption{
@@ -78,93 +107,11 @@ func runClient(connection string, server *grpc.Server) {
 	defer conn.Close()
 
 	c := proto.NewNodeManagementClient(conn)
-	answers := 0
-
-	for {
-		reply, err := c.RequestReply(context.Background(), &proto.Request{Message: "go Ahead", Id: *idf, Timestamp: 1})
-		if err != nil {
-			log.Fatalf("Reply cause fatal error: %v", err)
-		}
-		//We know this is cursed but it works.
-		//time.duration is in nanoseconds and
-		//we desire the order of maginitude to be in seconds
-		var magnitude int
-		magnitude = 1e9
-		waitTimer := (rand.Intn(4) + 1) * magnitude
-		fmt.Println(waitTimer / magnitude)
-
-		if "Go ahead" == reply.Message {
-			answers++
-			if answers == 2 {
-
-				thinkTimer := (rand.Intn(10) + 5) * magnitude
-				fmt.Println(thinkTimer / magnitude)
-				//Take critical Setion.
-				fmt.Print("I am the criticallest of sections\n")
-				time.Sleep(time.Duration(thinkTimer))
-				answers = 0
-			}
-		}
-		time.Sleep(time.Duration(waitTimer))
-		fmt.Printf("Server %d was told %s\n", *idf, reply.Message)
-	}
-}
-
-func (s *Server) RequestReply(ctx context.Context, in *proto.Request) (*proto.Reply, error) {
-	// If true this node accepts the requests replying go ahead
-	if s.ResolveRequest(in.Timestamp, in.Id) {
-		return &proto.Reply{Message: "Go ahead", Id: in.Id}, nil
-	} else {
-		return &proto.Reply{Message: "Don't go ahead", Id: in.Id}, nil
-	}
-}
-func (s *Server) ResolveRequest(ForeignTimestamp int64, foreignID int64) (Accaptance bool) {
-	fmt.Printf("Server %d got contacted by server %d\n", s.id, foreignID)
-	for s.usingCriticalSection {
-		time.Sleep(time.Millisecond * 500)
-	}
-	return true
-
-}
-
-// type Node struct {
-// 	id                   int64
-// 	timestamp            int64
-// 	ipList               []int
-// 	usingCriticalSection bool
-// }
-
-// var (
-// 	nodePort = flag.Int("nPort", 0, "ode port number")
-// 	idflag   = flag.Int("nId", 0, "Node id")
-// 	//serverPort = flag.Int("sPort", 0, "server port number (should match the port used for the server)")
-// )
-
-// func (n *Node) nodemain() {
-// 	flag.Parse()
-
-// }
-
-func GetPorts() []string {
-	content, err := ioutil.ReadFile("ports.txt")
+	message := fmt.Sprintf("Saying hi from %d", *idf)
+	reply, err := c.RequestReply(context.Background(), &proto.Request{Message: message, Id: *idf, Timestamp: 1})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Reply cause fatal error: %v", err)
 	}
-	portString := bytes.NewBuffer(content).String()
-	portArr := strings.Split(portString, ",")
+	fmt.Println(reply.Message)
 
-	return portArr
 }
-
-// func (n *Node) createNode() {
-
-// }
-
-// func (n *proto.NodeManagementClient) RequestReply(ctx context.Context, in *proto.Request) (*proto.Reply, error) {
-// 	// If true this node accepts the requests replying go ahead
-// 	if n.ResolveRequest(in.Id, in.Timestamp) {
-// 		return &proto.Reply{Message: "Go ahead", Id: in.Id}, nil
-// 	}
-// 	return nil, nil
-
-//}
